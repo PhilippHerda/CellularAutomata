812725aee25ac595ec90168cb463fb2faa02c4a1
Philipp Herda <philippherda@hotmail.com>
2021-09-06 23:35:19 +0200

    added a Menu the Quit-Button should not only close the application but also stop the thread

--- src/view/Window.java
+++ src/view/Window.java
@@ -4,8 +4,13 @@ import model.Game;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.Serial;
 
 public class Window extends Canvas {
+        @Serial
+        private static final long serialVersionUID = -784868000698187371L;
+
+    private JMenuItem quit;
 
         public Window(int width, int height, String title, Game game) {
             JFrame frame = new JFrame(title);
@@ -18,8 +23,21 @@ public class Window extends Canvas {
             frame.setResizable(false);
             frame.setLocationRelativeTo(null);
             frame.add(game);
+            frame.setJMenuBar(getJMenuBar());
             frame.setVisible(true);
             game.start();
         }
 
+        private JMenuBar getJMenuBar() {
+            JMenuBar jMenuBar = new JMenuBar();
+            JMenu jMenu = new JMenu("Menu");
+            quit = new JMenuItem("Quit");
+            jMenu.add(quit);
+            jMenuBar.add(jMenu);
+            return  jMenuBar;
+        }
+
+    public JMenuItem getQuit() {
+        return quit;
+    }
 }
