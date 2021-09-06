285a5809bb1449fd4604658ef3897831ed42b379
Philipp Herda <philippherda@hotmail.com>
2021-09-06 23:40:03 +0200

    implemented a prototype of a cellular automaton

--- src/model/Game.java
+++ src/model/Game.java
@@ -1,6 +1,7 @@
 package model;
 
 import controller.Handler;
+import view.Window;
 
 import java.awt.*;
 import java.awt.image.BufferStrategy;
@@ -12,7 +13,7 @@ public class Game extends Canvas implements Runnable{
     @Serial
     private static final long serialVersionUID = 533351226053614578L;
 
-    public static final int WIDTH = 1280, HEIGHT = WIDTH / 12 * 9;
+    public static final int WIDTH = 1000, HEIGHT = WIDTH;
 
     private Thread thread; //single threaded game not recommended!
     private boolean running = false;
@@ -21,23 +22,61 @@ public class Game extends Canvas implements Runnable{
     private Handler handler;
 
     public Game() {
-        new view.Window(WIDTH, HEIGHT, "Let´s build a Game!", this);
-
         handler = new Handler();
+
+        Window window = new view.Window(WIDTH, HEIGHT, "Let´s build a Game!", this);
+        window.getQuit().addActionListener(e -> {
+            System.exit(0);
+            stop();
+        });
+
         r = new Random();
-/*
+
+        /*
         for(int i = 0; i<50; i++) {
             handler.addObject(new Player(r.nextInt(WIDTH),r.nextInt(HEIGHT), ID.PLAYER));
             //handler.addObject(new Player(0,0, ID.PLAYER));
         }
         */
-        for(int i = 1; i<11; i++) {
-            for(int j = 1; j<11; j++) {
-                handler.addObject(new Block(i*35,j*35, ID.BLOCK));
+        /*
+        for(int i = 1; i<WIDTH/3; i++) {
+            for(int j = 1; j<HEIGHT/3; j++) {
+                handler.addObject(new Block(i*3,j*3, ID.BLOCK));
             }
         }
+        */
+        //generateBlockMatrix(100,100);
+        calculateRule(100,100);
 
+    }
 
+    private Block[][] generateBlockMatrix(int x, int y) {
+        Block[][] gameMatrix = new Block[x][y];
+        for(int i = 0; i < x; i++) {
+            for(int j = 0; j < y; j++) {
+                Block block = new Block(j+(9*i),i+(9*j), ID.BLOCK);
+                gameMatrix[i][j] = block;
+                handler.addObject(block);
+            }
+        }
+        return gameMatrix;
+    }
+
+    private Block[][] calculateRule(int x, int y) {
+        Block[][] matrix = generateBlockMatrix(x,y);
+        matrix[x/2][0].setActive(true);
+        for(int i = 1; i<x-1; i++) {
+            for(int j = 1; j<y-1; j++) {
+                if((matrix[i-1][j-1].isActive() && !matrix[i][j-1].isActive() && !matrix[i+1][j-1].isActive()) ||
+                        (!matrix[i-1][j-1].isActive() && matrix[i][j-1].isActive() && matrix[i+1][j-1].isActive()) ||
+                        (!matrix[i-1][j-1].isActive() && matrix[i][j-1].isActive() && !matrix[i+1][j-1].isActive()) ||
+                        (!matrix[i-1][j-1].isActive() && !matrix[i][j-1].isActive() && matrix[i+1][j-1].isActive())
+                ) {
+                    matrix[i][j].setActive(true);
+                }
+            }
+        }
+        return matrix;
     }
 
     public static void main(String[] args) {
